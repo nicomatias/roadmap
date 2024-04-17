55d828d83e239c9bd36fafba2ad710bb5e9b7cd2
Nico Matias <nllanosb@gmail.com>
2024-04-17 14:32:06 -0400

    15

---
+++ 15 - ASINCRONÍA/scripts/async.js
@@ -0,0 +1,14 @@
+function myDisplayer(some) {
+  document.getElementById("demo").innerHTML = some;
+}
+
+function myFirst() {
+  myDisplayer("Hello");
+}
+
+function mySecond() {
+  myDisplayer("Goodbye");
+}
+
+myFirst();
+mySecond();
